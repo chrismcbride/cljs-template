6c97baab1ba61193d9f2c060ff42aa5842936c99
chrismcbride <cmcbride@rjmetrics.com>
2012-04-19 18:53:28 -0300

    Update to clojure 1.4

--- src/leiningen/new/cljs_template/project.clj
+++ src/leiningen/new/cljs_template/project.clj
@@ -1,7 +1,7 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://exampl.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir-cljs "0.3.0"]
                  [jayq "0.1.0-alpha1"]
                  [fetch "0.1.0-alpha2"]
